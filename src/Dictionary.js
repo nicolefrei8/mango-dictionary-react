834d693e328758aeff5d3d22bf64dc28cb7232e3
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2022-03-24 23:23:07 +1030

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, {useState}from 'react';
 import axios from 'axios';
+import Results from './Results';
+
 
 export default function Dictionary(){
     const [keyword, setKeyword]= useState("");
+    const [results, setResults]= useState(null);
    
     
 function handleResponse(response){
-    console.log(response.data);
+    setResults(response.data[0]);
 }    
 
 function search(event){
 event.preventDefault();
-alert(`Searching for ${keyword}`);
+
 let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
 axios.get(apiUrl).then(handleResponse)
 }
@@ -23,7 +26,7 @@ return(
     <form onSubmit={search}>
        <input type="search"  onChange={keywordChange}/>
     </form>
-
+<Results results={results} />
 </div>
 )
 
